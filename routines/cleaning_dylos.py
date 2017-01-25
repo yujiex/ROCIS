662c2e4c73b00029e45aada208941cea3f650348
yujiex <shenqing1225@gmail.com>
2017-01-25 12:34:23 -0500

    combine separate, reformat, convert_unit into one step in function cleaning in cleaning_dylos.py

--- routines/cleaning_dylos.py
+++ routines/cleaning_dylos.py
@@ -1,4 +1,9 @@
 import re
+import glob
+import util
+import pandas as pd
+
+step_size = 100
 
 def format_match(string):
     pattern = re.compile('^([0-9]{2}/){2}[0-9]{2} [0-9]{2}:[0-9]{2}, *[0-9]{0,12}, *[0-9]{0,6}\r\n')
@@ -10,8 +15,43 @@ def cleaning(path, outpath):
         lines = rd.readlines()
     length = len(lines)
     datalines = ['Date/Time,Small,Large\n']
+    multiplier = 1.0
     for i in range(length):
+        if 'Particles per cubic foot\r\n' in lines[i]:
+            unit = 'Particles per cubic foot'
+            multiplier = 0.01
         if (format_match(lines[i])):
             datalines.append(lines[i])
     with open (outpath, 'w+') as wt:
         wt.write(''.join(datalines))
+    if multiplier != 1:
+        change_unit(outpath, multiplier)
+    return multiplier
+
+def change_unit(f, m):
+    df = pd.read_csv(f)
+    categories = list(df)
+    categories.remove('Date/Time')
+    for cate in categories:
+        df[cate] = df[cate].map(lambda x: int(round(x * m, 0)))
+    df.to_csv(f, index=False)
+    return
+
+def test_cleaning():
+    files = glob.glob(util.get_path('Dylos', 'raw_data', 'all') + '*.[a-z][a-z][a-z]')
+    files = files[:101]
+    mlines = ['filename,multiplier\n']
+    for i, f in enumerate(files):
+        if i % step_size == 0:
+            print i
+        m = cleaning(f, f.replace('raw_data', 'reform_'))
+        mlines.append('{0},{1}\n'.format(f[f.rfind('/') + 1:], m))
+    with open (util.get_path('Dylos', 'reform_', 'all') + 'summary/m.csv', 'w+') as wt:
+        wt.write(''.join(mlines))
+    return
+
+def main():
+    test_cleaning()
+    return
+    
+main()
--- routines/util.py
+++ routines/util.py
@@ -4,9 +4,12 @@ import pandas as pd
 def parent_dir(dirname):
     return dirname[:dirname.find('/routines')]
 
-def get_path(sensor, step, cohort):
-    return '{0}/DataBySensor/{1}/{2}/round_{3}/'.format(parent_dir(os.getcwd()), sensor, step, cohort)
-
+def get_path(sensor, step, cohort=None):
+    if cohort is None:
+        return '{0}/DataBySensor/{1}/{2}/'.format(parent_dir(os.getcwd()), sensor, step)
+    else:
+        return '{0}/DataBySensor/{1}/{2}/round_{3}/'.format(parent_dir(os.getcwd()), sensor, step, cohort)
+    
 def get_homeid_dict():
     df_lookup = pd.read_csv(os.getcwd() + '/input/ROCIS  LCMP Participants by Cohort_03-15-2016.csv')
     home_id_dict = dict(zip(df_lookup['INITIALS'], df_lookup['HOME ID CORRECT']) + zip(df_lookup['HOME ID CORRECT'], df_lookup['HOME ID CORRECT']) + zip(df_lookup['LAST'], df_lookup['HOME ID CORRECT']))
